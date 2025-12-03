701bedffd15507e25885981cb386bf2c5ad1a03f
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2025-12-03 15:12:00 +0300

    fix

---
+++ main.py
@@ -0,0 +1,43 @@
+import pymysql
+import connecting_database as conn_BD
+from connecting_database import dict_query as dt
+
+
+host, user, password, database = conn_BD.connecting_database()
+connection = False
+try:
+    # Подключение к базе данных
+    connection = pymysql.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
+
+    with connection.cursor() as cursor:
+        # # Создание новой записи
+        # sql = "INSERT INTO Authors (Name, Age, City, Price) VALUES (%s, %s, %s, %s)"
+        # cursor.execute(sql, ('Иван', 25, 'Волжский', 313))
+        # connection.commit()  # Сохранение изменений
+
+        # Получение данных
+        sql = f"{dt['Название животного на Л']}"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+
+
+except pymysql.MySQLError as text:
+    result = None
+    print(f'Ошибка: {text}')
+
+finally:
+    if connection:
+        connection.close()
+if result is None:
+    print(f'Ошибка соединения')
+
+elif result:
+    for tuple_ in result:
+        print(tuple_)
+else:
+    print(f'Нет данных')
