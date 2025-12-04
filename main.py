7b7bb680b4f05ad666fd3746e4e273c8a34a6c5d
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2025-12-04 16:11:31 +0300

    fix

--- main.py
+++ main.py
@@ -1,43 +1,63 @@
 import pymysql
 import connecting_database as conn_BD
 from connecting_database import dict_query as dt
+from connecting_database import creating_tables as table
 
 
 host, user, password, database = conn_BD.connecting_database()
 connection = False
 try:
-    # Подключение к базе данных
     connection = pymysql.connect(
         host=host,
         user=user,
         password=password,
         database=database
     )
+    requests = table()
 
     with connection.cursor() as cursor:
-        # # Создание новой записи
-        # sql = "INSERT INTO Authors (Name, Age, City, Price) VALUES (%s, %s, %s, %s)"
-        # cursor.execute(sql, ('Иван', 25, 'Волжский', 313))
-        # connection.commit()  # Сохранение изменений
-
-        # Получение данных
-        sql = f"{dt['Название животного на Л']}"
-        cursor.execute(sql)
-        result = cursor.fetchall()
+        for request in requests:
+            if request:
+                cursor.execute(request)
+        connection.commit()
 
 
 except pymysql.MySQLError as text:
-    result = None
     print(f'Ошибка: {text}')
 
-finally:
-    if connection:
-        connection.close()
-if result is None:
-    print(f'Ошибка соединения')
+# finally:
+#     if connection:
+#         connection.close()
+# if result is None:
+#     print(f'Ошибка соединения')
+#
+# elif result:
+#     for tuple_ in result:
+#         print(tuple_)
+# else:
+#     print(f'Нет данных')
 
-elif result:
-    for tuple_ in result:
-        print(tuple_)
-else:
-    print(f'Нет данных')
+# try:
+#     # Подключение к базе данных
+#     connection = pymysql.connect(
+#         host=host,
+#         user=user,
+#         password=password,
+#         database=database
+#     )
+#
+#     with connection.cursor() as cursor:
+#         # # Создание новой записи
+#         # sql = "INSERT INTO Authors (Name, Age, City, Price) VALUES (%s, %s, %s, %s)"
+#         # cursor.execute(sql, ('Иван', 25, 'Волжский', 313))
+#         # connection.commit()  # Сохранение изменений
+#
+#         # Получение данных
+#         sql = f"{dt['Название животного на Л']}"
+#         cursor.execute(sql)
+#         result = cursor.fetchall()
+#
+#
+# except pymysql.MySQLError as text:
+#     result = None
+#     print(f'Ошибка: {text}')
